8e5d6028ed8db7b7aff06e2ed16717a19706f960
brett wingard <brett.wingard@lancasterco.com>
2017-06-07 15:33:39 -0400

    deleted one last bogus method.

--- ServiceTagPuller/Forms/Main Form/Form1.cs
+++ ServiceTagPuller/Forms/Main Form/Form1.cs
@@ -443,23 +443,6 @@ namespace ServiceTagPuller
             webBrowser1.Document.GetElementById("divOffice").InnerHtml = "";
         }
 
-        public void ShowMyDialogBox()
-        {
-            //Form2 testDialog = new Form2();
-
-            //// Show testDialog as a modal dialog and determine if DialogResult = OK.
-            //if (testDialog.ShowDialog(this) == DialogResult.OK)
-            //{
-            //    // Read the contents of testDialog's TextBox.
-            //    this.txtResult.Text = testDialog.TextBox1.Text;
-            //}
-            //else
-            //{
-            //    this.txtResult.Text = "Cancelled";
-            //}
-            //testDialog.Dispose();
-        }
-
         //work in progress.....
         private void checkLogs()
         {
