d5474326de49a8d6ea040f5e6bcb9faf6b90729f
Brizow <b.wingard@polydeckscreen.com>
2018-11-28 14:06:06 -0500

    Fixed issue with broken divs in the HTML frame.

--- ServiceTagPuller/Forms/VersionForm.Designer.cs
+++ ServiceTagPuller/Forms/VersionForm.Designer.cs
@@ -36,30 +36,27 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(16, 48);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 39);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(545, 330);
+            this.richTextBox1.Size = new System.Drawing.Size(410, 269);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.Size = new System.Drawing.Size(83, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Revision History";
             // 
             // closeBtn
             // 
-            this.closeBtn.Location = new System.Drawing.Point(463, 386);
-            this.closeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.closeBtn.Location = new System.Drawing.Point(347, 314);
             this.closeBtn.Name = "closeBtn";
-            this.closeBtn.Size = new System.Drawing.Size(100, 28);
+            this.closeBtn.Size = new System.Drawing.Size(75, 23);
             this.closeBtn.TabIndex = 2;
             this.closeBtn.Text = "Close Me";
             this.closeBtn.UseVisualStyleBackColor = true;
@@ -67,13 +64,12 @@
             // 
             // VersionForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(579, 422);
+            this.ClientSize = new System.Drawing.Size(434, 343);
             this.Controls.Add(this.closeBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "VersionForm";
             this.Text = "VersionForm";
             this.ResumeLayout(false);
